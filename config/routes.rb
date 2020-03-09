44f97113af3bdcc269b8d7df2876b47259347ceb
github username <ingrey1@gmail.com>
2020-03-09 14:30:21 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      
+      resources :books # all books
+      resources :users do 
+         resources :books, only: [:index, :show, :create, :destroy] do # user's own collection 
+           resources :chapters do # user's book's chapters 
+             resources :comments # book's chapter's comments 
+           end 
+         end 
+      end 
+    end 
+  end 
+
 end
