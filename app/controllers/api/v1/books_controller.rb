bcee89c84ade6510da132d3bf4c436bd4d8b1d5c
github username <ingrey1@gmail.com>
2020-03-12 17:10:06 -0700

    added render_my_Books to books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -8,6 +8,10 @@ class Api::V1::BooksController < ApplicationController
     
     def index
 
+      handle_auth { render_my_books }
+
+
+
     end 
 
     def create
@@ -30,6 +34,11 @@ class Api::V1::BooksController < ApplicationController
 
     def render_all_books
       render json: Book.all
+    end
+    
+    def render_my_books
+       user = User.find(params[:user_id])
+       render json: user.books.all
     end 
 
 end
