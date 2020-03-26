a8cc77ae558310b6d455a7817d91adafae8fd699
github username <ingrey1@gmail.com>
2020-03-26 08:29:35 -0700

    added create and destroy comments actions

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,6 +5,12 @@ class Api::V1::CommentsController < ApplicationController
     
     def create
 
+        handle_auth { render_create_comment }
+
+    end
+    
+    def destroy
+       handle_auth { render_destroy_comment }
     end 
 
     private
@@ -17,6 +23,22 @@ class Api::V1::CommentsController < ApplicationController
             render json: {errors: ["couldnt find comments for chapter #{chapter.id}"]}
         end 
     
+    end 
+
+    def render_create_comment
+       user = User.find(params[:user_id])
+       chapter = Chapter.find(params[:chapter_id])
+       content = params[:content]
+       comment = Comment.new(user: user, chapter: chapter, content: content)
+       if comment.save
+         render json: comment
+       else 
+         render json: {errors: ["failed to create comment"]}, status: 500
+       end 
+    end 
+
+    def render_destroy_comment
+       render json: {success: 'comment destroyed'}, status: 200
     end 
 
 end
